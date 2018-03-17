846f9bd16eaf9bdcf0f022c8d5205aa664fca2d2
SeanChen <livebreeze@gmail.com>
2018-03-17 16:52:24 +0800

    Composing components

--- ReactDemoWithMVC/Scripts/Tutorial.jsx
+++ ReactDemoWithMVC/Scripts/Tutorial.jsx
@@ -1,13 +1,36 @@
 ﻿var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
       </div>
-    );
-  }
+        );
+    }
 });
+
+var CommentList = React.createClass({
+    render: function () {
+        return (
+            <div className="commentList">
+                Hello, world! I am a CommentList.
+      </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+      </div>
+        );
+    }
+});
+
 ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
+    <CommentBox />,
+    document.getElementById('content')
 );
