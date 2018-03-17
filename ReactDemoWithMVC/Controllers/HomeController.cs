42db82bf8c8ead5a36bcef66ff4c989dee223ffc
SeanChen <livebreeze@gmail.com>
2018-03-17 22:36:34 +0800

    Adding new comments

--- ReactDemoWithMVC/Controllers/HomeController.cs
+++ ReactDemoWithMVC/Controllers/HomeController.cs
@@ -44,5 +44,14 @@ namespace ReactDemoWithMVC.Controllers
         {
             return Json(_comments, JsonRequestBehavior.AllowGet);
         }
+
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            // Create a fake ID for this comment
+            comment.Id = _comments.Count + 1;
+            _comments.Add(comment);
+            return Content("Success :)");
+        }
     }
 }
